05acd19b4350292b36e1db12831b36a405bc1a84
zulfirmn <firman.ninetynine@gmail.com>
2024-02-04 16:00:31 +0800

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -25,6 +25,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
@@ -96,7 +101,13 @@ const init = async () => {
       validator: CollaborationsValidator,
     },
   },
-  ]);
+  {
+    plugin: _exports,
+    options: {
+      service: ProducerService,
+      validator: ExportsValidator,
+    },
+  }]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
